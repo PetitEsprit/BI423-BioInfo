f8bf5ff45957072d3fbcdf4fb035be31f72c8a06
PetitEsprit <PetitEsprit@users.noreply.github.com>
2019-04-09 16:44:56 +0200

    gskew

---
+++ td8/gcskew.py
@@ -0,0 +1,52 @@
+import matplotlib.pyplot as plt
+
+# name = raw_input("Entrer un nom de fichier...")
+tf = input("Entrer une taille de fenetre...")
+f = open("sequence.fasta", "r")
+ligne = ""
+seq = ""
+i = 0
+countC = 0
+countG = 0
+skew = 0
+minSkew = 0
+maxSkew = 0
+
+f.readline()
+ligne = f.readline()
+while ligne != "":
+	seq =  seq + ligne[0:len(ligne)-1]
+	ligne = f.readline()
+
+while i < len(seq):
+	if(seq[i] == "C"):
+		countC = countC + 1
+	if(seq[i] == "G"):
+		countG = countG + 1
+	if(i % tf == 0):
+		if(countC != 0 or countG !=0):
+			skew = float(countG - countC)/ float(countC + countG)
+			plt.scatter(i, skew, c = "black", s=10)
+			
+			if skew > maxSkew:
+				maxSkew = skew
+			if skew < minSkew:
+				minSkew = skew
+		else:
+			skew = 0
+		countC = countG = 0
+		print "skew: ", skew
+	i = i + 1
+
+if(countC != 0 or countG !=0):
+			skew = float(countG - countC)/ float(countC + countG)
+			plt.scatter(i, skew, c = "black", s=10)
+
+
+plt.title("GC Skew") # titre du graphique
+plt.xlabel("Position") # label/nom de l'axe des abscisses
+plt.ylabel("Skew") # label/nom de l'axe des des ordonnees
+plt.xlim(0, len(seq)) # taille/longueur de l'axe des "x"
+plt.ylim(minSkew, maxSkew)# taille/longueur de l'axe des "y"
+
+plt.show() # affichage du graphique
